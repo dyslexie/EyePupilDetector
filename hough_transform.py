e156fd47fff4ef753ab904a4a60a64fc09544388
dyslexie <dag.rybak@gmail.com>
2018-08-29 21:26:14 +0200

    added missing hough transform

--- hough_transform.py
+++ hough_transform.py
@@ -8,6 +8,44 @@ def perform_hough_transform_on_file(filename, min_rad, max_rad, file_bool):
     new_image = cv2.imread(filename)
     return perform_hough_transform(new_image, min_rad,max_rad)
 
+def perform_hough_transforms_and_return_circles(new_image, min_rad, max_rad):
+    old_image = new_image.copy()
+
+    gray_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe_image = clahe.apply(gray_image)
+
+    blur_image = cv2.medianBlur(clahe_image, 7)
+
+    # src_gray: Input image (grayscale, blur)
+    # circles: A vector that stores sets of 3 values: x_{c}, y_{c}, r for each detected circle.
+    # CV_HOUGH_GRADIENT: Define the detection method. Currently this is the only one available in OpenCV
+    # dp = 1: The inverse ratio of resolution
+    # min_dist = src_gray.rows/8: Minimum distance between detected centers
+    # param_1 = 200: Upper threshold for the internal Canny edge detector
+    # param_2 = 100*: Threshold for center detection.
+    # min_radius = 0: Minimum radio to be detected. If unknown, put zero as default.
+    # max_radius = 0: Maximum radius to be detected. If unknown, put zero as default
+
+    hough_circles = cv2.HoughCircles(blur_image,
+                                     cv2.HOUGH_GRADIENT,
+                                     1,
+                                     20,
+                                     param1=50,
+                                     param2=30,
+                                     minRadius=min_rad,
+                                     maxRadius=max_rad)
+    
+    try:
+        for circle in hough_circles[0,:]:
+            cv2.circle(new_image,(circle[0],circle[1]),circle[2],(0,255,0),2)
+            cv2.circle(old_image,(circle[0],circle[1]),2,(0,0,255),3)
+    except:
+        print("NO PUPIL FOUND!")
+
+    return hough_circles
+
 def perform_hough_transform(new_image, min_rad, max_rad):
     old_image = new_image.copy()
 
